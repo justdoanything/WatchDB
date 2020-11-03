c306618c9f107b81cf63eb2cd40f31e435735206
82736 <82736@LCSC08V899.corp.lgcns.com>
2020-11-03 18:01:52 +0900

    Revert "Complete to make a structure"
    This reverts commit 9a28239ff1d31f2f1480d9cd3529d01d9f3e2896.

--- src/com/yong/Runner/Runner.java
+++ src/com/yong/Runner/Runner.java
@@ -1,10 +1,22 @@
 package com.yong.Runner;
 
+import org.apache.log4j.Logger;
+
+import com.yong.Watch.Watcher;
+
 public class Runner {
 
+	private static Logger logger = Logger.getLogger(Runner.class);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		logger.info("[ Watch DB Start ]");
+		Configuration.initialize(args);
+		
+		Watcher watcher = new Watcher();
+		watcher.execute();
+		
+		logger.info("[ Watch DB End ]");
 	}
 
 }
---
+++ src/com/yong/Watch/Watcher.java
@@ -0,0 +1,21 @@
+package com.yong.Watch;
+
+import org.apache.log4j.Logger;
+
+public class Watcher {
+	private Logger logger = Logger.getLogger(this.getClass());
+	
+	public Watcher() {
+		
+	}
+	
+	public void execute() {
+		WatchService watchSvc = new WatchService();
+		
+		watchSvc.test();
+		
+		watchSvc.finalize();
+		
+	}
+
+}
