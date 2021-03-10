6519719a155e9fb9268aca3fe34b5eaef502b7a4
82736 <82736@LCSC08V899.corp.lgcns.com>
2021-03-10 13:31:43 +0900

    Push Batch Class (Runnable)

--- src/com/yong/Common/BatchExecutor.java
+++ src/com/yong/Common/BatchExecutor.java
@@ -5,28 +5,30 @@ import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
 
+import com.yong.Runner.Configuration;
+import com.yong.Watch.Watcher;
+
 public abstract class BatchExecutor implements Runnable
 {
 	Logger logger = Logger.getLogger(this.getClass());
 	
-	/**
-	 * 파일의 크기를 감시하는 주기(millisecond)
-	 * - context.properties 에서 set
-	 */
-	long fileDelay = 5000;
-
 	/** 
 	 * 지정된 시간마다 수행됨
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-//		logger.info(String.format("start.. DataFileTask(path=%s, name=*%s, delay=%dms)", filePath, fileName, fileDelay, charset));
+		int fileDelay = 50000;
+		try { fileDelay = Configuration.getInt(MsgCode.CONF_EXE_INTERVAL); } catch (Exception ex) {	ex.printStackTrace(); }
 		
-		new Timer().schedule(new TimerTask() {
+		logger.info(String.format("Batch start →→→ delay=%dms", fileDelay));
+		
+		new Timer().schedule(new TimerTask() {	
+			
 			public void run() {
-				try {		
-
+				try {
+					Watcher watcher = new Watcher();
+					watcher.execute();
 				}
 				catch(Exception e) {
 					e.printStackTrace();
